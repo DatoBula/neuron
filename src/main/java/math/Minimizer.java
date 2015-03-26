6373d6d9586a8304c5f94ce7898818a5892d132e
Davit Abulashvili <datobula@gmail.com>
2015-03-26 18:00:08 +0400

    ref

--- src/main/java/math/Minimizer.java
+++ src/main/java/math/Minimizer.java
@@ -2,5 +2,5 @@ package math;
 
 public interface Minimizer {
 
-	public double findMinimum(Function function, int parametersCount);
+	public double findMinimum(Function function, int dimentions);
 }
